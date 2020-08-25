856c6bc74cf548c77659ad74ac82728317a8a482
Joudy Nader <Joudy.Nader@irisa.fr>
2020-08-25 16:56:49 +0200

    Upgrade AprilTag detection task. At this stage it contains: 1) Creating the detector in the first codel. 2) Detecting tags in left undistorted image:    i) If nothing is detected, the buffer containing previously detected tags is freed,    ii) Otherwise, the saved parameters for each tag are : timestamp, tag ID, center, corners, pose, message.
    TODO:
    1) Add pre and post-tf for pose transformation.
    2) Add output port of type sequence<Tag> to have information about detected tags.
    3) Have undistort and AprilTag detection in a single task.
    4) Add options for display : enable/disable and frame_rate.
    5) Add all control functions. (IBVS/PBVS?, pre-post tf, ...)

--- codels/T265_apriltag_detection_codels.cc
+++ codels/T265_apriltag_detection_codels.cc
@@ -9,6 +9,15 @@ vpDetectorAprilTag::vpPoseEstimationMethod poseEstimationMethod = vpDetectorApri
 vpDetectorAprilTag *detector;
 vpCameraParameters cam_undistort;
 std::vector<vpHomogeneousMatrix> cMo_vec;
+std::vector<std::vector<vpImagePoint> > tag_corners;
+std::vector<int> tag_ids;
+vpImagePoint center;
+vpTranslationVector cto;
+vpQuaternionVector cqo;
+
+
+long tmp_sec;
+double tmp_nsec;
 
 /** Codel init_detector of task apriltag_detection.
  *
@@ -17,8 +26,7 @@ std::vector<vpHomogeneousMatrix> cMo_vec;
  */
 genom_event
 init_detector(const T265_realsense_grabber *rs_grabber,
-              double tag_size, double quad_decimate,
-              optional_T265_corners *tag_corners,
+              double *tag_size, sequence_apriltag_tag *detected_tags,
               const genom_context self)
 {
   vpCameraParameters cam_left = rs_grabber->g.getCameraParameters(RS2_STREAM_FISHEYE, vpCameraParameters::ProjWithKannalaBrandtDistortion, 1);
@@ -26,18 +34,16 @@ init_detector(const T265_realsense_grabber *rs_grabber,
 
   detector = new vpDetectorAprilTag(tagFamily);
 
-  detector->setAprilTagQuadDecimate(quad_decimate);
+  detector->setAprilTagQuadDecimate(1);
   detector->setAprilTagPoseEstimationMethod(poseEstimationMethod);
   detector->setAprilTagNbThreads(2);
   detector->setDisplayTag(false, -1 < 0 ? vpColor::none : vpColor::getColor(-1), 2);
   detector->setZAlignedWithCameraAxis(false);
 
-  tag_corners->_present = false;
-  for(int i = 0 ; i < 4; i++)
-  {
-    tag_corners->_value._buffer[i].i = 0.;
-    tag_corners->_value._buffer[i].j = 0.;
-  }
+  *tag_size = 0.08; // Default value for tag size.
+
+  detected_tags->_buffer = NULL;
+  detected_tags->_length = 0;
 
   return T265_loop;
 }
@@ -49,14 +55,91 @@ init_detector(const T265_realsense_grabber *rs_grabber,
  * Yields to T265_pause_loop, T265_stop.
  */
 genom_event
-loop_detector(const T265_vp_image *I_left_undistorted,
+loop_detector(const T265_vp_image *I_left_undistorted, double tag_size,
+              sequence_apriltag_tag *detected_tags,
               const genom_context self)
 {
   cMo_vec.clear();
 
-  detector->detect(I_left_undistorted->I, 0.08, cam_undistort, cMo_vec);
-  if(cMo_vec.size() != 0)
-    std::cout << cMo_vec.size() << std::endl;
+  detector->detect(I_left_undistorted->I, tag_size, cam_undistort, cMo_vec);
+
+  if(cMo_vec.size() == 0) // No tags detected in this iteration, release the buffer of detected_tags.
+  {
+    if(detected_tags->_length != 0)
+    {
+      detected_tags->_maximum = 0;
+      detected_tags->_length = 0;
+      detected_tags->_buffer = NULL;
+    }
+  }
+
+  else // Tag(s) detected.
+  {
+    if(detected_tags->_length != 0) // Releasing already existing buffer.
+    {
+      // for(int i = 0; i < detected_tags->_length; i++)
+      //   delete detected_tags->_buffer[i].message._value;
+      delete [] detected_tags->_buffer;
+      detected_tags->_buffer = NULL;
+    }
+
+    detected_tags->_maximum = cMo_vec.size();
+    detected_tags->_length = 0;
+
+    tag_corners = detector->getTagsCorners(); // Get all tags corners.
+    tag_ids = detector->getTagsId(); // Get all tags IDs.
+
+    if(detected_tags->_buffer == NULL)
+    {
+      detected_tags->_buffer = new apriltag_tag[detected_tags->_maximum];
+    }
+
+    // Filling detected_tags data structure.
+    for(int i = 0; i < cMo_vec.size(); i++)
+    {
+      tmp_sec    = I_left_undistorted->timestamp / 1000;
+      tmp_nsec = ((long)I_left_undistorted->timestamp % 1000) * 1000000;
+
+      // Save timestamp of image as timestamp of apriltag.
+      detected_tags->_buffer[i].ts.sec  = static_cast<int32_t>(tmp_sec);
+      detected_tags->_buffer[i].ts.nsec = static_cast<int32_t>(tmp_nsec);
+
+      // Save ID of tag.
+      detected_tags->_buffer[i].id = tag_ids[i];
+
+      // Save center of tag.
+      center = detector->getCog(i);
+      detected_tags->_buffer[i].center._value.u = center.get_i();
+      detected_tags->_buffer[i].center._value.v = center.get_j();
+
+      // Save corners.
+      for(int j = 0; j < 4; j++)
+      {
+        detected_tags->_buffer[i].corners_pos._value[j].u = tag_corners[i][j].get_i();
+        detected_tags->_buffer[i].corners_pos._value[j].v = tag_corners[i][j].get_j();
+      }
+
+      // Save apriltag's pose.
+      cMo_vec[i].extract(cto);
+      cMo_vec[i].extract(cqo);
+
+      detected_tags->_buffer[i].pos._value.x = cto[0];
+      detected_tags->_buffer[i].pos._value.y = cto[1];
+      detected_tags->_buffer[i].pos._value.z = cto[2];
+
+      detected_tags->_buffer[i].att._value.qx = cqo[0];
+      detected_tags->_buffer[i].att._value.qy = cqo[1];
+      detected_tags->_buffer[i].att._value.qz = cqo[2];
+      detected_tags->_buffer[i].att._value.qw = cqo[3];
+
+      // Save apriltag's message.
+      strcpy(detected_tags->_buffer[i].message._value, detector->getMessage(i).c_str());
+
+      detected_tags->_length++;
+    }
+  }
+
+  // std::cout << cMo_vec.size() << " " << detected_tags->_length << " " << detected_tags->_maximum << std::endl;
 
   return T265_pause_loop;
 }
@@ -68,8 +151,10 @@ loop_detector(const T265_vp_image *I_left_undistorted,
  * Yields to T265_ether.
  */
 genom_event
-kill_detector(const genom_context self)
+kill_detector(sequence_apriltag_tag *detected_tags,
+              const genom_context self)
 {
   detector = NULL;
+
   return T265_ether;
 }
