8071d01e2710bf04e0c1c778fabffaeb693e686c
Joudy Nader <Joudy.Nader@irisa.fr>
2020-08-26 09:45:27 +0200

    Directly copy pose_data to port instead of copying every field in data structure.

--- codels/T265_pose_port_refresh_codels.cc
+++ codels/T265_pose_port_refresh_codels.cc
@@ -24,7 +24,7 @@ init_port(const T265_odom_state *odom_state, const genom_context self)
   s->aacc._present = true;
 
   if(odom_state->write(self))
-    std::cout << "entered if" << std::endl;
+    std::cout << "Error" << std::endl;
 
   return T265_loop;
 }
@@ -42,39 +42,42 @@ refresh_pose(const T265_realsense_grabber *rs_grabber,
              const T265_odom_state *odom_state,
              const genom_context self)
 {
-  double start = vpTime::measureTimeMs();
-
   or_pose_estimator_state *s = odom_state->data(self);
 
-  s->pos._present = true;
-  s->pos._value.x = pose_data->pos._value.x;
-  s->pos._value.y = pose_data->pos._value.y;
-  s->pos._value.z = pose_data->pos._value.z;
+  // s->ts.sec  = pose_data->ts.sec;
+  // s->ts.nsec = pose_data->ts.nsec;
 
-  s->att._present = true;
-  s->att._value.qx = pose_data->att._value.qx;
-  s->att._value.qy = pose_data->att._value.qy;
-  s->att._value.qz = pose_data->att._value.qz;
+  // s->pos._present = true;
+  // s->pos._value.x = pose_data->pos._value.x;
+  // s->pos._value.y = pose_data->pos._value.y;
+  // s->pos._value.z = pose_data->pos._value.z;
 
-  s->vel._present = true;
-  s->vel._value.vx = pose_data->vel._value.vx;
-  s->vel._value.vy = pose_data->vel._value.vy; 
-  s->vel._value.vz = pose_data->vel._value.vz;
+  // s->att._present = true;
+  // s->att._value.qx = pose_data->att._value.qx;
+  // s->att._value.qy = pose_data->att._value.qy;
+  // s->att._value.qz = pose_data->att._value.qz;
 
-  s->avel._present = true;
-  s->avel._value.wx = pose_data->avel._value.wx;
-  s->avel._value.wy = pose_data->avel._value.wy; 
-  s->avel._value.wz = pose_data->avel._value.wz;
+  // s->vel._present = true;
+  // s->vel._value.vx = pose_data->vel._value.vx;
+  // s->vel._value.vy = pose_data->vel._value.vy;
+  // s->vel._value.vz = pose_data->vel._value.vz;
 
-  s->acc._present = true;
-  s->acc._value.ax = pose_data->acc._value.ax;
-  s->acc._value.ay = pose_data->acc._value.ay; 
-  s->acc._value.az = pose_data->acc._value.az;
+  // s->avel._present = true;
+  // s->avel._value.wx = pose_data->avel._value.wx;
+  // s->avel._value.wy = pose_data->avel._value.wy;
+  // s->avel._value.wz = pose_data->avel._value.wz;
 
-  s->aacc._present = true;
-  s->aacc._value.awx = pose_data->aacc._value.awx;
-  s->aacc._value.awy = pose_data->aacc._value.awy; 
-  s->aacc._value.awz = pose_data->aacc._value.awz;
+  // s->acc._present = true;
+  // s->acc._value.ax = pose_data->acc._value.ax;
+  // s->acc._value.ay = pose_data->acc._value.ay;
+  // s->acc._value.az = pose_data->acc._value.az;
+
+  // s->aacc._present = true;
+  // s->aacc._value.awx = pose_data->aacc._value.awx;
+  // s->aacc._value.awy = pose_data->aacc._value.awy;
+  // s->aacc._value.awz = pose_data->aacc._value.awz;
+
+  *s = *pose_data;
 
   if(odom_state->write(self))
     std::cout << "Error" << std::endl;
