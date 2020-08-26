1d9672a61e2ef39eeac3fdb16c4636b0ccf1798d
Joudy Nader <Joudy.Nader@irisa.fr>
2020-08-26 11:55:48 +0200

    Add start and pause codels.

--- codels/T265_codels.cc
+++ codels/T265_codels.cc
@@ -1,3 +1,31 @@
 #include "acT265.h"
 
 #include "T265_c_types.h"
+
+
+/* --- Function start_publishing ---------------------------------------- */
+
+/** Codel start_publish of function start_publishing.
+ *
+ * Returns genom_ok.
+ */
+genom_event
+start_publish(bool *is_publishing, const genom_context self)
+{
+  *is_publishing = true;
+  return genom_ok;
+}
+
+
+/* --- Function pause_publishing ---------------------------------------- */
+
+/** Codel pause_publish of function pause_publishing.
+ *
+ * Returns genom_ok.
+ */
+genom_event
+pause_publish(bool *is_publishing, const genom_context self)
+{
+  *is_publishing = false;
+  return genom_ok;
+}
