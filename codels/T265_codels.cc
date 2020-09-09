0e4d4fed40a3e1cc9efdd7f2bfc8f83f8dd22b5e
Joudy Nader <Joudy.Nader@irisa.fr>
2020-09-09 17:35:43 +0200

    Modify start_publishing() function codel. When called, it will define the callback and the configuration and start the vpRealSense2 object.
    Note that, the callback now captures IDS as value because it's already a pointer.

--- codels/T265_codels.cc
+++ codels/T265_codels.cc
@@ -2,6 +2,10 @@
 #include "codels.h"
 #include "T265_c_types.h"
 
+long sec;
+double timestamp, nsec;
+vpTranslationVector ctw;
+vpQuaternionVector cqw;
 
 /* --- Function start_publishing ---------------------------------------- */
 
@@ -10,9 +14,133 @@
  * Returns genom_ok.
  */
 genom_event
-start_publish(bool *is_publishing, const genom_context self)
+start_publish(T265_ids *ids, const genom_context self)
 {
-  *is_publishing = true;
+  rs2::config cfg;
+  // Configuring pipeline streams.
+  cfg.enable_stream(RS2_STREAM_POSE, RS2_FORMAT_6DOF);
+  cfg.enable_stream(RS2_STREAM_FISHEYE, 1, RS2_FORMAT_Y8);
+  cfg.enable_stream(RS2_STREAM_FISHEYE, 2, RS2_FORMAT_Y8);
+  // cfg.enable_stream(RS2_STREAM_ACCEL);
+  // cfg.enable_stream(RS2_STREAM_GYRO);
+
+  // Callback to be executed when a frame arrives.
+  std::function<void(const rs2::frame)> callback = [=](const rs2::frame& frame)
+  {
+    timestamp = frame.get_timestamp();
+
+    if (rs2::frameset fs = frame.as<rs2::frameset>())
+    {
+      // With callbacks, all synchronized stream will arrive in a single frameset
+      rs2::video_frame left_frame = fs.get_fisheye_frame(1);
+      size_t size = left_frame.get_width() * left_frame.get_height();
+      memcpy(ids->I_left->I.bitmap, left_frame.get_data(), size);
+      ids->I_left->timestamp = timestamp;
+
+      rs2::video_frame right_frame = fs.get_fisheye_frame(2);
+      size = right_frame.get_width() * right_frame.get_height();
+      memcpy(ids->I_right->I.bitmap, right_frame.get_data(), size);
+      ids->I_right->timestamp = timestamp;
+
+      ids->image_count++; // Images received succesfully.
+
+      // In case there's a pose in the frameset.
+      rs2_pose pose_data = fs.get_pose_frame().get_pose_data();
+      ctw = vpTranslationVector(static_cast<double>(pose_data.translation.x),
+                              static_cast<double>(pose_data.translation.y),
+                              static_cast<double>(pose_data.translation.z));
+
+      cqw = vpQuaternionVector(static_cast<double>(pose_data.rotation.x),
+                            static_cast<double>(pose_data.rotation.y),
+                            static_cast<double>(pose_data.rotation.z),
+                            static_cast<double>(pose_data.rotation.w));
+
+      // Timestamp.
+      ids->poseref_odo_sensor->timestamp = timestamp;
+
+      // Pose
+      ids->poseref_odo_sensor->pose.buildFrom(ctw, cqw);
+
+      // Velocity
+      ids->poseref_odo_sensor->vel[0] = static_cast<double>(pose_data.velocity.x);
+      ids->poseref_odo_sensor->vel[1] = static_cast<double>(pose_data.velocity.y);
+      ids->poseref_odo_sensor->vel[2] = static_cast<double>(pose_data.velocity.z);
+
+      ids->poseref_odo_sensor->vel[3] = static_cast<double>(pose_data.angular_velocity.x);
+      ids->poseref_odo_sensor->vel[4] = static_cast<double>(pose_data.angular_velocity.y);
+      ids->poseref_odo_sensor->vel[5] = static_cast<double>(pose_data.angular_velocity.z);
+
+      // Acceleration
+      ids->poseref_odo_sensor->acc[0] = static_cast<double>(pose_data.acceleration.x);
+      ids->poseref_odo_sensor->acc[1] = static_cast<double>(pose_data.acceleration.y);
+      ids->poseref_odo_sensor->acc[2] = static_cast<double>(pose_data.acceleration.z);
+
+      ids->poseref_odo_sensor->acc[3] = static_cast<double>(pose_data.angular_acceleration.x);
+      ids->poseref_odo_sensor->acc[4] = static_cast<double>(pose_data.angular_acceleration.y);
+      ids->poseref_odo_sensor->acc[5] = static_cast<double>(pose_data.angular_acceleration.z);
+
+      ids->pose_count++;
+      ids->poseref_odo_sensor->tracker_confidence = pose_data.tracker_confidence;
+    }
+    else
+    {
+      rs2_pose pose_data = frame.as<rs2::pose_frame>().get_pose_data();
+
+      // Stream that bypass synchronization (such as IMU, Pose, ...) will produce single frames
+      ctw = vpTranslationVector(static_cast<double>(pose_data.translation.x),
+                              static_cast<double>(pose_data.translation.y),
+                              static_cast<double>(pose_data.translation.z));
+
+      cqw = vpQuaternionVector(static_cast<double>(pose_data.rotation.x),
+                            static_cast<double>(pose_data.rotation.y),
+                            static_cast<double>(pose_data.rotation.z),
+                            static_cast<double>(pose_data.rotation.w));
+
+      // Timestamp.
+      ids->poseref_odo_sensor->timestamp = timestamp;
+
+      // Pose
+      ids->poseref_odo_sensor->pose.buildFrom(ctw, cqw);
+
+      // Velocity
+      ids->poseref_odo_sensor->vel[0] = static_cast<double>(pose_data.velocity.x);
+      ids->poseref_odo_sensor->vel[1] = static_cast<double>(pose_data.velocity.y);
+      ids->poseref_odo_sensor->vel[2] = static_cast<double>(pose_data.velocity.z);
+
+      ids->poseref_odo_sensor->vel[3] = static_cast<double>(pose_data.angular_velocity.x);
+      ids->poseref_odo_sensor->vel[4] = static_cast<double>(pose_data.angular_velocity.y);
+      ids->poseref_odo_sensor->vel[5] = static_cast<double>(pose_data.angular_velocity.z);
+
+      // Acceleration
+      ids->poseref_odo_sensor->acc[0] = static_cast<double>(pose_data.acceleration.x);
+      ids->poseref_odo_sensor->acc[1] = static_cast<double>(pose_data.acceleration.y);
+      ids->poseref_odo_sensor->acc[2] = static_cast<double>(pose_data.acceleration.z);
+
+      ids->poseref_odo_sensor->acc[3] = static_cast<double>(pose_data.angular_acceleration.x);
+      ids->poseref_odo_sensor->acc[4] = static_cast<double>(pose_data.angular_acceleration.y);
+      ids->poseref_odo_sensor->acc[5] = static_cast<double>(pose_data.angular_acceleration.z);
+
+      ids->pose_count++;
+      ids->poseref_odo_sensor->tracker_confidence = pose_data.tracker_confidence;
+    }
+  };
+
+  // Start publishing => open() vpRealSense2 object and start.
+  ids->rs_grabber->g.open(cfg, callback);
+
+  ids->I_left->I.resize(ids->rs_grabber->g.getIntrinsics(RS2_STREAM_FISHEYE, 1).height,
+                        ids->rs_grabber->g.getIntrinsics(RS2_STREAM_FISHEYE, 1).width);
+
+  ids->I_right->I.resize(ids->rs_grabber->g.getIntrinsics(RS2_STREAM_FISHEYE, 2).height,
+                        ids->rs_grabber->g.getIntrinsics(RS2_STREAM_FISHEYE, 2).width);
+
+  ids->nb_display_coefficient = 1;
+  ids->display_enabled = false;
+  ids->detection_enabled = false;
+
+  // This will enable pose_port_refresh to be executed. (not directly skipped)
+  ids->is_publishing = true;
+
   return genom_ok;
 }
 
